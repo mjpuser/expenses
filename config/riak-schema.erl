2876b4f7a6ecfe186bc8c683bd6785156e8f5c1f
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2014-06-25 05:08:53 -0400

    fixed indentation on config

--- config/riak-schema.erl
+++ config/riak-schema.erl
@@ -1,43 +1,43 @@
 {
-    schema,
-    [
-        {version, "1.1"},
-        {n_val, 3},
-        {default_field, "value"},
-        {analyzer_factory, {erlang, text_analyzers, whitespace_analyzer_factory}}
-    ],
-    [
-				%% category
-				{field, [
-						{name, "category"},
-						{type, string},
-						{analyzer_factory, {erlang, text_analyzers, whitespace_analyzer_factory}}
-				]},
+	schema,
+	[
+		{version, "1.1"},
+		{n_val, 3},
+		{default_field, "value"},
+		{analyzer_factory, {erlang, text_analyzers, whitespace_analyzer_factory}}
+	],
+	[
+		%% category
+		{field, [
+			{name, "category"},
+			{type, string},
+			{analyzer_factory, {erlang, text_analyzers, whitespace_analyzer_factory}}
+		]},
 
-				%% name
-				{field, [
-						{name, "name"},
-						{type, string},
-						{analyzer_factory, {erlang, text_analyzers, whitespace_analyzer_factory}}
-				]},
+		%% name
+		{field, [
+			{name, "name"},
+			{type, string},
+			{analyzer_factory, {erlang, text_analyzers, whitespace_analyzer_factory}}
+		]},
 
-        %% amount
-        {field, [
-            {name, "amount"},
-            {type, integer}
-	       ]},
+		%% amount
+		{field, [
+			{name, "amount"},
+			{type, integer}
+		]},
 
-        %% date
-        {field, [
-            {name, "date"},
-            {type, date}
-        ]},
+		%% date
+		{field, [
+			{name, "date"},
+			{type, date}
+		]},
 
-        %% Everything else is a string
-        {dynamic_field, [
-            {name, "*"},
-            {type, string},
-            {analyzer_factory, {erlang, text_analyzers, whitespace_analyzer_factory}}
-        ]}
-    ]
+		%% Everything else is a string
+		{dynamic_field, [
+			{name, "*"},
+			{type, string},
+			{analyzer_factory, {erlang, text_analyzers, whitespace_analyzer_factory}}
+		]}
+	]
 }.
